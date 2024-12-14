eb8ea42b48b3fe52f1c4756413fbfc5261a5ef9b
Promise Nwabueze Igbojionu <promiseemmanuel@byui.edu>
2024-12-14 04:36:25 -0700

    week 06

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,7 +12,11 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
-
+        if (value == Data)
+        {
+            // ignore duplicates
+            return;
+        }
         if (value < Data)
         {
             // Insert to the left
@@ -34,12 +38,23 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
+        if (value == Data)
+            return true;
+
+        if (value < Data && Left != null)
+            return Left.Contains(value);
+        
+        if (value > Data && Right != null)    
+            return Right.Contains(value);
+
         return false;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
